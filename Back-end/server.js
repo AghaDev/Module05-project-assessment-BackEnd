44c49e21047a071141de3908dcfb3cdb32f6f4a6
AghaDev <mohamadagha2@gmail.com>
2024-02-05 15:28:15 +0200

    fixed a small change

--- Back-end/server.js
+++ Back-end/server.js
@@ -7,7 +7,6 @@ const userRoute = require('./Routes/userRoute.js');
 const productRoute = require('./Routes/productRoute.js');
 const orderRoute = require('./Routes/orderRoute.js');
 
-
 const app = express();
 const PORT = process.env.PORT;
 
@@ -15,34 +14,26 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.json());
 
-
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is running on ${process.env.PORT}`);
-    });
+    console.log(`Connected to MongoDB`);
+    startServer();
   })
   .catch((err) => {
     console.log("Error connecting to the database:", err);
   });
 
 function startServer() {
-  // Use the user route
-    app.use('/user', userRoute);
-    app.use('/api', productRoute);
-    app.use('/', orderRoute)
+  app.use('/user', userRoute);
+  app.use('/api', productRoute);
+  app.use('/order', orderRoute);
 
-
-
-
-  // Error handling middleware
   app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).json({ error: 'Internal Server Error' });
   });
 
-  // Start the server
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
